bef6ff959a5a0e71c343d6ec23d3d726628012b1
giovannialayan <giovannialayan@gmail.com>
2022-03-26 17:32:36 -0400

    simple models hw

---
+++ server/models/Dog.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const DogSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    breed: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: false,
+    },
+    age: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: false,
+    },
+    createdDate: {
+      type: Date,
+      default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Dog', DogSchema);
